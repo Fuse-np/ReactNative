15cd5fdd85f84f9aef3497ec8718114bd03fd547
Fuse-np <118034100+Fuse-np@users.noreply.github.com>
2023-07-22 11:34:32 +0700

    useState commit

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Cat from './compoents/Cat';
+import { StyleSheet, Text, View } from "react-native";
+import ViewBoxesWithColorAndText from "./components/ViewBoxesWithColorAndText";
+import DisplayandImage from "./components/DisplayandImage";
+import LotsOfGreeting from "./components/LotsOfGreeting";
+import CustomText from "./components/CustomText";
+import Counter from "./components/Counter";
+import IncrementCounter from "./components/IncrementCounter";
+import MyInput from "./components/Myinput";
+import From from "./components/From";
 
 export default function App() {
   return (
-    <View >
-
-    <Cat/>
-   
+    <View style={styles.container}>
+      {/* <ViewBoxesWithColorAndText/> */}
+      {/* <DisplayandImage/> */}
+      {/* <LotsOfGreeting/> */}
+      {/* <CustomText/> */}
+      {/* <Counter/> */}
+      {/* <MyInput/> */}
+      {/* <IncrementCounter/> */}
+      <From/>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container:{
+     flex:1,   
+     justifyContent:'center',    
+     alignItems:'center'   
+  }
+ })
