59e62489992e5e3c6c62c03f2c554559e6978f07
Fuse-np <118034100+Fuse-np@users.noreply.github.com>
2023-08-05 09:51:12 +0700

    Practice

---
+++ compoents/TouchablePractice.js
@@ -0,0 +1,75 @@
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import React from "react";
+
+const TouchablePractice = () => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.buttonFacebookStyle}>
+        <Image
+          source={require("../assets/facebook.png")}
+          style={styles.buttonImageIconStyle}
+        />
+        <View style={styles.buttonIconSeparatorStyle} />
+        <Text style={styles.buttonTextStyle}>Login Using Facebook</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.buttonGPlusStyle}>
+        <Image
+          source={require("../assets/google-plus.png")}
+          style={styles.buttonImageIconStyle}
+        />
+        <View style={styles.buttonIconSeparatorStyle} />
+        <Text style={styles.buttonTextStyle}>Login Using Facebook</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default TouchablePractice;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10,
+    marginTop: 30,
+    padding: 30,
+  },
+  buttonGPlusStyle: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#dc4e41",
+    borderWidth: 0.5,
+    borderColor: "#fff",
+    height: 40,
+    borderRadius: 5,
+    margin: 5,
+  },
+  buttonFacebookStyle: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#485a96",
+    borderWidth: 0.5,
+    borderColor: "#fff",
+    height: 40,
+    borderRadius: 5,
+    margin: 5,
+  },
+  buttonImageIconStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: "stretch",
+  },
+  buttonTextStyle: {
+    color: "#fff",
+    marginBottom: 4,
+    marginLeft: 10,
+  },
+
+  buttonIconSeparatorStyle: {
+    backgroundColor: "#fff",
+    width: 1,
+    height: 40,
+  },
+});
